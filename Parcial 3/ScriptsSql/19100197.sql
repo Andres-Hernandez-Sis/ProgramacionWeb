bcb45c5b23c4a6267c9938eaf19eb270f0908fe0
Andres H <None>
2022-05-24 09:50:31 -0500

    Actualizacion de adaptacion a MySQL

--- Parcial 3/ScriptsSql/19100197.sql
+++ Parcial 3/ScriptsSql/19100197.sql
@@ -1,10 +1,8 @@
-Create database A19100197;
-Use A19100197; 
-
-/*Se agrego la A al principio pues no dejaba ponerle solo numeros de nombre*/
+Create database `19100197`;
+Use `19100197`; 
 
 Create table Producto(
-id int not null identity(1,1), 
+id int not null AUTO_INCREMENT PRIMARY KEY, 
 nombre varchar(30) not null,
 descripcion varchar(45) null,
 cantidad int not null,
@@ -14,10 +12,8 @@ categoria varchar(20) not null check (categoria='Lacteos' OR categoria='Carnes'
 									OR categoria='Frutas' OR categoria='Verduras'
 									OR categoria='Cereales' OR categoria='Leguminosas' OR categoria='Cosas del hogar'),
 codigo_barra varchar(30) not null,
-importado varchar(2),
+importado varchar(2)
 );
-
-SET IDENTITY_INSERT Producto ON;
 
 insert into Producto(id,nombre,descripcion,cantidad,proveedor,caducidad,categoria,codigo_barra,importado) values
 					(1, 'Atun', '130gr', 5, 'Tuny', '22/04/23' , 'Carnes', '1223456789', 'Si');
@@ -33,6 +29,3 @@ insert into Producto(id,nombre,descripcion,cantidad,proveedor,caducidad,categori
 
 insert into Producto(id,nombre,descripcion,cantidad,proveedor,caducidad,categoria,codigo_barra,importado) values
 					(5, 'Jabon en polvo', '500gr', 7, 'Foca', '22/04/23', 'Cosas del hogar', '5678912340', 'Si');
-
-insert into Producto(id,nombre,descripcion,cantidad,proveedor,caducidad,categoria,codigo_barra,importado) values
-					(6, 'Cereal de maiz', '600 gr', 25, 'Kellogs', '22/04/23', 'Cereales', '6789123456', 'No');
